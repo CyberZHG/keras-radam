1acae89003dcfbfa1234db9d92d19c11b3984cc8
nilay <nilay>
2019-08-17 01:23:33 +0530

    rectified nesterov adam

--- keras_radam/optimizers.py
+++ keras_radam/optimizers.py
@@ -3,11 +3,10 @@ from .backend import backend as K
 from .legacy import interfaces
 
 
-__all__ = ['RNAdam']
+__all__ = ['RAdam','RNAdam']
 
-
-class RNAdam(keras.optimizers.Optimizer):
-    """RNAdam optimizer.
+class RAdam(keras.optimizers.Optimizer):
+    """RAdam optimizer.
     # Arguments
         lr: float >= 0. Learning rate.
         beta_1: float, 0 < beta < 1. Generally close to 1.
@@ -18,6 +17,94 @@ class RNAdam(keras.optimizers.Optimizer):
     # References
         - [Adam - A Method for Stochastic Optimization](https://arxiv.org/abs/1412.6980v8)
         - [On The Variance Of The Adaptive Learning Rate And Beyond](https://arxiv.org/pdf/1908.03265v1.pdf)
+    """
+
+    def __init__(self, lr=0.001, beta_1=0.9, beta_2=0.999,
+                 epsilon=None, decay=0., weight_decay=0., **kwargs):
+        super(RAdam, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.lr = K.variable(lr, name='lr')
+            self.beta_1 = K.variable(beta_1, name='beta_1')
+            self.beta_2 = K.variable(beta_2, name='beta_2')
+            self.decay = K.variable(decay, name='decay')
+            self.weight_decay = K.variable(weight_decay, name='weight_decay')
+        if epsilon is None:
+            epsilon = K.epsilon()
+        self.epsilon = epsilon
+        self.initial_decay = decay
+        self.initial_weight_decay = weight_decay
+
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations, K.dtype(self.decay))))
+
+        t = K.cast(self.iterations, K.floatx()) + 1
+
+        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='m_' + str(i)) for (i, p) in enumerate(params)]
+        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='v_' + str(i)) for (i, p) in enumerate(params)]
+
+        self.weights = [self.iterations] + ms + vs
+
+        beta_1_t = K.pow(self.beta_1, t)
+        beta_2_t = K.pow(self.beta_2, t)
+
+        sma_inf = 2.0 / (1.0 - self.beta_2) - 1.0
+        sma_t = sma_inf - 2.0 * t * beta_2_t / (1.0 - beta_2_t)
+
+        for p, g, m, v in zip(params, grads, ms, vs):
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
+
+            m_hat_t = m_t / (1.0 - beta_1_t)
+            v_hat_t = K.sqrt(v_t / (1.0 - beta_2_t) + self.epsilon)
+
+            r_t = K.sqrt((sma_t - 4.0) / (sma_inf - 4.0) *
+                         (sma_t - 2.0) / (sma_inf - 2.0) *
+                         sma_inf / sma_t)
+
+            p_t = K.switch(sma_t > 5, r_t * m_hat_t / v_hat_t, m_hat_t)
+
+            if self.initial_weight_decay > 0:
+                p_t += self.weight_decay * p
+
+            p_t = p - lr * p_t
+
+            self.updates.append(K.update(m, m_t))
+            self.updates.append(K.update(v, v_t))
+            new_p = p_t
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {
+            'lr': float(K.get_value(self.lr)),
+            'beta_1': float(K.get_value(self.beta_1)),
+            'beta_2': float(K.get_value(self.beta_2)),
+            'decay': float(K.get_value(self.decay)),
+            'weight_decay': float(K.get_value(self.weight_decay)),
+            'epsilon': self.epsilon,
+        }
+        base_config = super(RAdam, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+class RNAdam(keras.optimizers.Optimizer):
+    """RNAdam optimizer.
+    # Arguments
+        lr: float >= 0. Learning rate.
+        beta_1: float, 0 < beta < 1. Generally close to 1.
+        beta_2: float, 0 < beta < 1. Generally close to 1.
+        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
+        schedule_decay: float, 0 < schedule_decay < 1.
     """
 
     def __init__(self, lr=0.002, beta_1=0.9, beta_2=0.999,
