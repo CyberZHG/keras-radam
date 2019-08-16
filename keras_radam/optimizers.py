51e42afb1c032c47a0b32fbb3366163af9d93ebd
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-16 19:35:10 +0800

    Add optimizer

---
+++ keras_radam/optimizers.py
@@ -0,0 +1,91 @@
+from .backend import keras
+from .backend import backend as K
+
+
+__all__ = ['RAdam']
+
+
+class RAdam(keras.optimizers.Optimizer):
+    """RAdam optimizer.
+
+    # Arguments
+        learning_rate: float >= 0. Learning rate.
+        beta_1: float, 0 < beta < 1. Generally close to 1.
+        beta_2: float, 0 < beta < 1. Generally close to 1.
+        epsilon: float >= 0. Fuzz factor. If `None`, defaults to `K.epsilon()`.
+        decay: float >= 0. Learning rate decay over each update.
+    # References
+        - [Adam - A Method for Stochastic Optimization](https://arxiv.org/abs/1412.6980v8)
+        - [RAdam](https://arxiv.org/pdf/1908.03265v1.pdf)
+    """
+
+    def __init__(self, learning_rate=0.001, beta_1=0.9, beta_2=0.999,
+                 epsilon=None, decay=0., **kwargs):
+        learning_rate = kwargs.pop('lr', None) or learning_rate
+        super(RAdam, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.learning_rate = K.variable(learning_rate, name='learning_rate')
+            self.beta_1 = K.variable(beta_1, name='beta_1')
+            self.beta_2 = K.variable(beta_2, name='beta_2')
+            self.decay = K.variable(decay, name='decay')
+        if epsilon is None:
+            epsilon = K.epsilon()
+        self.epsilon = epsilon
+        self.initial_decay = decay
+
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.learning_rate
+        if self.initial_decay > 0:
+            lr = lr * (1. / (1. + self.decay * K.cast(self.iterations, K.dtype(self.decay))))
+
+        t = K.cast(self.iterations, K.floatx()) + 1
+
+        ms = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='m_' + str(i)) for (i, p) in enumerate(params)]
+        vs = [K.zeros(K.int_shape(p), dtype=K.dtype(p), name='v_' + str(i)) for (i, p) in enumerate(params)]
+
+        self.weights = [self.iterations] + ms + vs
+
+        beta_1_t = K.pow(self.beta_1, t)
+        beta_2_t = K.pow(self.beta_2, t)
+
+        sma_inf = 2.0 / (1.0 - self.beta_2) - 1.0
+        sma_t = sma_inf - 2.0 * t * beta_2_t / (1.0 - beta_2_t)
+
+        for p, g, m, v in zip(params, grads, ms, vs):
+            m_t = (self.beta_1 * m) + (1. - self.beta_1) * g
+            v_t = (self.beta_2 * v) + (1. - self.beta_2) * K.square(g)
+
+            m_hat_t = m_t / (1.0 - beta_1_t)
+            v_hat_t = K.sqrt(v_t / (1.0 - beta_2_t) + self.epsilon)
+
+            r_t = K.sqrt((sma_t - 4.0) / (sma_inf - 4.0) *
+                         (sma_t - 2.0) / (sma_inf - 2.0) *
+                         sma_inf / sma_t + self.epsilon)
+
+            p_t = p - lr * K.switch(sma_t > 5, r_t * m_hat_t / (K.sqrt(v_hat_t + self.epsilon)), m_hat_t)
+
+            self.updates.append(K.update(m, m_t))
+            self.updates.append(K.update(v, v_t))
+            new_p = p_t
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {
+            'learning_rate': float(K.get_value(self.learning_rate)),
+            'beta_1': float(K.get_value(self.beta_1)),
+            'beta_2': float(K.get_value(self.beta_2)),
+            'decay': float(K.get_value(self.decay)),
+            'epsilon': self.epsilon,
+        }
+        base_config = super(RAdam, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
---
+++ tests/test_optimizers.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+from unittest import TestCase
+
+import numpy as np
+
+from keras_radam.backend import keras
+from keras_radam import RAdam
+
+
+class TestRAdam(TestCase):
+
+    @staticmethod
+    def gen_linear_model() -> keras.models.Model:
+        model = keras.models.Sequential()
+        model.add(keras.layers.Dense(
+            input_shape=(17,),
+            units=3,
+            bias_constraint=keras.constraints.max_norm(),
+            name='Dense',
+        ))
+        model.compile(RAdam(decay=1e-4), loss='mse')
+        return model
+
+    @staticmethod
+    def gen_linear_data(w=None) -> (np.ndarray, np.ndarray):
+        np.random.seed(0xcafe)
+        x = np.random.standard_normal((4096 * np.random.randint(20, 30), 17))
+        if w is None:
+            w = np.random.standard_normal((17, 3))
+        y = np.dot(x, w)
+        return x, y, w
+
+    def test_fit(self):
+        x, y, w = self.gen_linear_data()
+        model = self.gen_linear_model()
+
+        model_path = os.path.join(tempfile.gettempdir(), 'test_accumulation_%f.h5' % np.random.random())
+        model.save(model_path)
+        model = keras.models.load_model(model_path, custom_objects={'RAdam': RAdam})
+
+        model.fit(x, y, epochs=5)
+
+        model_path = os.path.join(tempfile.gettempdir(), 'test_accumulation_%f.h5' % np.random.random())
+        model.save(model_path)
+        model = keras.models.load_model(model_path, custom_objects={'RAdam': RAdam})
+
+        x, y, w = self.gen_linear_data(w)
+        predicted = model.predict(x)
+        self.assertLess(np.max(np.abs(predicted - y)), 1e-3)
