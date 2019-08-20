dbc4fd48c0a81dc0aca9e880e65f0676cdb86abd
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-21 00:49:24 +0800

    Add sparse part of the tensorflow optimizer

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setup(
     name='keras-rectified-adam',
-    version='0.5.0',
+    version='0.6.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-radam',
     license='MIT',
