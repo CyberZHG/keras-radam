8498d9cac21e7aa5223d8afcc8246ccbb2b93ad1
Zhao HG <853842+CyberZHG@users.noreply.github.com>
2019-08-22 22:29:41 +0800

    Update optimizer_v2.py

--- keras_radam/optimizer_v2.py
+++ keras_radam/optimizer_v2.py
@@ -195,7 +195,7 @@ class RAdam(OptimizerV2):
                             (sma_t - 2.0) / (sma_inf - 2.0) *
                             sma_inf / sma_t)
 
-        var_t = tf.where(sma_t > 5.0, r_t * m_corr_t / v_corr_t, m_corr_t)
+        var_t = tf.where(sma_t >= 5.0, r_t * m_corr_t / v_corr_t, m_corr_t)
 
         if self._initial_weight_decay > 0.0:
             var_t += self._get_hyper('weight_decay', var_dtype) * var
