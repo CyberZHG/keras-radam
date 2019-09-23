0419d8b386c9b20d9b0a6811592569061844f47a
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-09-23 21:45:26 +0800

    Update travis configuration

--- setup.py
+++ setup.py
@@ -1,29 +1,27 @@
-import codecs
+import os
 import re
-
+import codecs
 from setuptools import setup, find_packages
-from os import path
 
-here = path.abspath(path.dirname(__file__))
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(*parts):
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return reader.read()
 
 
 def get_requirements(*parts):
-    with codecs.open(path.join(here, *parts), 'r', 'utf8') as fp:
-        return list(map(lambda x: x.strip(), fp.readlines()))
-
-
-def read(*parts):
-    with codecs.open(path.join(here, *parts), 'r', 'utf8') as fp:
-        return fp.read()
+    with codecs.open(os.path.join(current_path, *parts), 'r', 'utf8') as reader:
+        return list(map(lambda x: x.strip(), reader.readlines()))
 
 
 def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_file = read_file(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')
 
 
 setup(
@@ -35,7 +33,7 @@ setup(
     author='CyberZHG',
     author_email='CyberZHG@users.noreply.github.com',
     description='RAdam implemented in Keras & TensorFlow',
-    long_description=read('README.md'),
+    long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
     install_requires=get_requirements('requirements.txt'),
     classifiers=[
