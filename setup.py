4942b70af62caf06d00f958b88f25f2411050199
Romain Brault <mail@romainbrault.com>
2019-09-23 10:12:46 +0200

    add version

--- setup.py
+++ setup.py
@@ -1,29 +1,46 @@
 import codecs
+import re
+
 from setuptools import setup, find_packages
+from os import path
 
-with codecs.open('README.md', 'r', 'utf8') as reader:
-    long_description = reader.read()
+here = path.abspath(path.dirname(__file__))
 
 
-with codecs.open('requirements.txt', 'r', 'utf8') as reader:
-    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+def get_requirements(*parts):
+    with codecs.open(path.join(here, *parts), 'r', 'utf8') as fp:
+        return list(map(lambda x: x.strip(), fp.readlines()))
+
+
+def read(*parts):
+    with codecs.open(path.join(here, *parts), 'r', 'utf8') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name='keras-rectified-adam',
-    version='0.14.0',
+    version=find_version('keras_radam', '__init__.py'),
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-radam',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@users.noreply.github.com',
     description='RAdam implemented in Keras & TensorFlow',
-    long_description=long_description,
+    long_description=read('README.md'),
     long_description_content_type='text/markdown',
-    install_requires=install_requires,
-    classifiers=(
+    install_requires=get_requirements('requirements.txt'),
+    classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
