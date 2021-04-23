03353b041fbce62fd8b81298bf9ae2fa1e034a86
Mohammed Innat <innat1994@gmail.com>
2021-04-23 22:13:40 +0600

    update tf imports

--- keras_radam/optimizer_v2.py
+++ keras_radam/optimizer_v2.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.python.keras.optimizer_v2.optimizer_v2 import OptimizerV2
-from tensorflow.python import ops, math_ops, state_ops, control_flow_ops
+from tensorflow.python.os import math_ops, state_ops, control_flow_ops
 from tensorflow.python.keras import backend as K
 
 __all__ = ['RAdam']
